3c7bcfe2d28540e9497ca11e5d870994c08041ba
Muhammadqodir <m.qodir777@gmail.com>
2023-05-17 14:10:45 +0300

    fixed 2 package

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,18 @@ window.addEventListener('resize', function (event) {
 
 function updateSlider() {
   var prefix = "";
+  var type = ".png";
   if (bootstrapDetectBreakpoint()["index"] < 2) {
-    prefix = "_mobile";
+    prefix = "m";
     $(".partners .title").css("display", "none");
+    var type = ".jpg";
   } else {
     $(".partners .title").css("display", "block");
+    var type = ".png";
     prefix = "";
   }
   for (let index = 1; index < 5; index++) {
-    $("#slide" + index).attr("src", "assets/slider-items/" + index + prefix + ".png");
+    $("#slide" + index).attr("src", "assets/slider-items/" + prefix + index + type);
   }
 }
 
